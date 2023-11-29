ef96a0b45788eb8d3b07cd149b249b2f678f6dad
T4li0n-Th3Sh4d0w <123408992+T4li0n-Th3Sh4d0w@users.noreply.github.com>
2023-11-30 00:14:23 +0100

    Update addgame.py

--- addgame.py
+++ addgame.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, String, Integer, ForeignKey
+from sqlalchemy import create_engine, Column, String, Integer, ForeignKey, func
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 import random
