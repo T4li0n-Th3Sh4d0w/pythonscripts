5081f9be5ccc4f13b97fe38ee914c3dee5a680d4
T4li0n-Th3Sh4d0w <123408992+T4li0n-Th3Sh4d0w@users.noreply.github.com>
2023-11-30 00:01:50 +0100

    Update addlobby.py

--- addlobby.py
+++ addlobby.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, String, Integer, ForeignKey
+from sqlalchemy import create_engine, Column, String, Integer, ForeignKey, func
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 Base = declarative_base()
